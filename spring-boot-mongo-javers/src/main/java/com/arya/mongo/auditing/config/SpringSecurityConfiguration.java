bf1d32dfcfe20ddd125efaed65dc39eba871d5b6
rahul-ghadge <rahul.g5557@gmail.com>
2020-08-20 14:02:39 +0530

    Added spring security in memory authentication in envers and javers modules

---
+++ spring-boot-mongo-javers/src/main/java/com/arya/mongo/auditing/config/SpringSecurityConfiguration.java
@@ -0,0 +1,40 @@
+package com.arya.mongo.auditing.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SpringSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+		auth.inMemoryAuthentication()
+		.withUser("user").password("user123").roles("USER")
+		.and()
+		.withUser("admin").password("admin123").roles("ADMIN");
+
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+				.antMatchers("/admin").hasRole("ADMIN")
+				.antMatchers("/user").hasAnyRole("ADMIN", "USER")
+				.antMatchers("/").permitAll().and().formLogin()
+				.and()
+				.logout().permitAll();
+	}
+
+	@Bean
+	public PasswordEncoder getPasswordEncoder() {
+		return NoOpPasswordEncoder.getInstance();
+	}
+
